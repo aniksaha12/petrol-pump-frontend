ec832cd40e6ded6f8fb4c5fb5e4488bb317fc9ad
ANIK SAHA <46953050+aniksaha12@users.noreply.github.com>
2023-12-21 00:55:56 +0530

    login signup

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,131 +1,118 @@
-"use client";
+'use client';
 
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export default function LoginPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const router = useRouter();
+export default function LoginPage(){
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const router = useRouter()
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-  const supabase = createClientComponentClient();
 
-  const handleSignIn = async () => {
-    await supabase.auth.signInWithPassword({
-      email,
-      password,
-    });
-    router.refresh();
-    setEmail('')
-    setPassword('')
-  };
+    const supabase = createClientComponentClient();
 
-  return (
-    <main>
-      <section className="bg-gray-50 dark:bg-gray-900">
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <a
-            href="#"
-            className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-          >
-            {/* <img
-              className="w-8 h-8 mr-2"
-              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-              alt="logo"
-            /> */}
-            Petrol-Pump
-          </a>
-          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Sign in to your account
-              </h1>
-              <form className="space-y-4 md:space-y-6" action="#">
-                <div>
-                  <label
-                    htmlFor="email"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Your email
-                  </label>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="name@company.com"
-                    required=""
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </div>
-                <div>
-                  <label
-                    htmlFor="password"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="••••••••"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    required=""
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </div>
-                <div className="flex items-center justify-between">
-                  <div className="flex items-start">
-                    <div className="flex items-center h-5">
-                      <input
-                        id="remember"
-                        aria-describedby="remember"
-                        type="checkbox"
-                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                        required=""
-                      />
-                    </div>
-                    <div className="ml-3 text-sm">
-                      <label
-                        htmlFor="remember"
-                        className="text-gray-500 dark:text-gray-300"
-                      >
-                        Remember me
-                      </label>
-                    </div>
-                  </div>
-                  <a
-                    href="#"
-                    className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
-                  >
-                    Forgot password?
-                  </a>
-                </div>
-                <button
-                  type="submit"
-                  onClick={handleSignIn}
-                  className="w-full text-white  bg-blue-700 hover:bg-blue-800 bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+    useEffect(() => {
+        async function getUser(){
+            const {data: {user}} = await supabase.auth.getUser()
+            setUser(user)
+            setLoading(false)
+        }
+
+        getUser();
+    }, [])
+
+
+    const handleSignUp = async () => {
+        const res = await supabase.auth.signUp({
+            email,
+            password,
+            options: {
+                emailRedirectTo: `${location.origin}/auth/callback`
+            }
+        })
+        setUser(res.data.user)
+        router.refresh();
+        setEmail('')
+        setPassword('')
+    }
+
+    const handleSignIn = async () => {
+        const res = await supabase.auth.signInWithPassword({
+            email,
+            password
+        })
+        setUser(res.data.user)
+        router.refresh();
+        setEmail('')
+        setPassword('')
+    }
+
+    const handleLogout = async () => {
+        await supabase.auth.signOut();
+        router.refresh();
+        setUser(null)
+    }
+
+    console.log({loading, user})
+
+    if (loading){
+        return <h1>loading..</h1>
+    }
+
+    if (user){
+        return (
+            <div className="h-screen flex flex-col justify-center items-center bg-gray-100">
+            <div className="bg-white dark:bg-gray-900 p-8 rounded-lg shadow-md w-96 text-center">
+                <h1 className="mb-4 text-xl font-bold text-gray-700 dark:text-gray-300">
+                    You're already logged in
+                </h1>
+                <button 
+                    onClick={handleLogout}
+                    className="w-full p-3 rounded-md bg-red-500 text-white hover:bg-red-600 focus:outline-none"
                 >
-                  Sign in
+                    Logout
                 </button>
-                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                  Don’t have an account yet?{" "}
-                  <a
-                    href="#"
-                    className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                  >
-                    Sign up
-                  </a>
-                </p>
-              </form>
             </div>
-          </div>
         </div>
-      </section>
-    </main>
-  );
+        )
+    }
+
+    return (
+        <main className="h-screen flex items-center justify-center bg-gray-800 p-6">
+        <div className="bg-gray-900 p-8 rounded-lg shadow-md w-96">
+        <input 
+            type="email" 
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            className="mb-4 w-full p-3 rounded-md border border-gray-700 bg-gray-800 text-white placeholder-gray-500 focus:outline-none focus:border-blue-500"
+        />
+        <input 
+            type="password" 
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            className="mb-4 w-full p-3 rounded-md border border-gray-700 bg-gray-800 text-white placeholder-gray-500 focus:outline-none focus:border-blue-500"
+        />
+        <button 
+            onClick={handleSignUp}
+            className="w-full mb-2 p-3 rounded-md bg-blue-600 text-white hover:bg-blue-700 focus:outline-none"
+        >
+            Sign Up
+        </button>
+        <button 
+            onClick={handleSignIn}
+            className="w-full p-3 rounded-md bg-gray-700 text-white hover:bg-gray-600 focus:outline-none"
+        >
+            Sign In
+        </button>
+        </div>
+        </main>
+    )
+
 }
