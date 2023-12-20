6017fd843ed167389da7a275e2a45d1893fa9e70
ANIK SAHA <46953050+aniksaha12@users.noreply.github.com>
2023-12-21 01:15:43 +0530

    cookie implements

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,15 @@
 import Image from "next/image";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 
-export default function Home() {
+export default  async function Home() {
+
+  const cookieStore = cookies();
+  const supabase = createServerComponentClient({ cookies: () => cookieStore });
+
+  const {data: {user}} = await supabase.auth.getUser()
+  console.log({user})
+
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
       <div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex">
